3198d5747e72baef299a93b65c92419294a912f7
warner1334 <58993802+warner1334@users.noreply.github.com>
2021-06-02 14:40:22 -0700

    Update XYCoordinates.java

--- Amazons Code (Ours)/XYCoordinates.java
+++ Amazons Code (Ours)/XYCoordinates.java
@@ -1,16 +1,35 @@
 package ubc.cosc322;
 
-public class XYCoordinates {
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class XYCoordinates{
 	public int x, y;
-	
 	// constructor
-	public XYCoordinates(int x, int y) {
+	public XYCoordinates(int x, int y)  {
 		this.x = x;
 		this.y = y;
 	}
+	
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+	public ArrayList<Integer> getXY() {
+		ArrayList<Integer> xy = new ArrayList<Integer>();
+		xy.add(y);
+		xy.add(x);
+		return xy;
+	}
+	
+	
 	
 	@Override
 	public String toString() {
 		return x + "," + y;
 	}
+
+	
 }
