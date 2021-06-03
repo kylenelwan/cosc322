34e6e9ddc63238da6e27ea5fd7ab35b44fb02075
warner1334 <58993802+warner1334@users.noreply.github.com>
2021-06-03 16:29:19 -0700

    Update RandomPlayer.java

--- Amazons Code (Ours)/RandomPlayer.java
+++ Amazons Code (Ours)/RandomPlayer.java
@@ -10,10 +10,16 @@ public class RandomPlayer {
 	public Move ranMove(ArrayList<Move> moves) {
 		Random rand = new Random();
 		this.moves = moves;
+		if(moves.size() > 0 ) {
 		int randomNum = rand.nextInt(moves.size());
 		Move ranMove = moves.get(randomNum);
+		
 		return ranMove;
-	
+		}
+		else {
+			return null;
+		}
+		
 	}
 
 }
