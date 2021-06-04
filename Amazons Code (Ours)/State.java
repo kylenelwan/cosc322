3c2122f0b45046a72b4986f6b40a2ad11c1b0e14
warner1334 <58993802+warner1334@users.noreply.github.com>
2021-06-04 16:06:49 -0700

    Create State.java

---
+++ Amazons Code (Ours)/State.java
@@ -0,0 +1,45 @@
+package ubc.cosc322;
+
+import java.awt.List;
+import java.util.ArrayList;
+
+public class State {
+	
+	    Board board;
+	    int playerNo;
+	    int visitCount;
+	    double winScore;
+	    AllPossibleActions actions;
+	    // copy constructor, getters, and setters
+	    public State(Board board, int playerNo, int visitCount, double winScore) {
+	    	this.board = new Board(board);
+	    	this.playerNo = playerNo;
+	    	this.visitCount = visitCount;
+	    	this.winScore = winScore;
+	    }
+	    public ArrayList<State> getAllPossibleStates() {
+	        // constructs a list of all possible states from current state
+	    	ArrayList<Move> allActions = new ArrayList<Move>();
+	    	if(playerNo == 1) {
+	    		allActions = actions.getAllBlackQueens(board);
+	    	}else {
+	    		allActions = actions.getAllWhiteQueens(board);
+	    	}
+	    	ArrayList<State> allStates = new ArrayList<State>();
+	    	for(Move moves: allActions) {
+	    		Board newBoard = new Board(board);
+	    		newBoard.updateState(moves.queenPos, moves.queenNext, moves.arrowPos);
+	    		playerNo = playerNo == 2? 1 :2;
+	    		allStates.add(new State(newBoard, playerNo, visitCount, winScore));
+	    	}
+	    	return allStates;
+	    	
+	    }
+	    public void randomPlay() {
+	        /* get a list of all possible positions on the board and 
+	           play a random move */
+	    	ArrayList<State> allPos = getAllPossibleStates();
+	    	
+	    }
+	}
+
