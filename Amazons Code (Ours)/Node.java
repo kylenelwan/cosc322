d953ca1f5e36c83f126170e03db1767cc0d61d67
lorapokrajac <77463562+lorapokrajac@users.noreply.github.com>
2021-06-05 11:59:21 -0700

    Add files via upload

---
+++ Amazons Code (Ours)/Node.java
@@ -0,0 +1,35 @@
+/*
+ * Sources used:
+ * https://www.baeldung.com/java-monte-carlo-tree-search
+ */
+package ubc.cosc322;
+
+import java.util.ArrayList;
+
+public class Node {
+	State state;
+	Node parent;
+	ArrayList<Node> Child;
+	public State getState() {
+		return state;
+	}
+	public void setState(State state) {
+		this.state = state;
+	}
+	public Node getParent() {
+		return parent;
+	}
+	public void setParent(Node parent) {
+		this.parent = parent;
+	}
+	public ArrayList<Node> getChild() {
+		return Child;
+	}
+	public void setChild(ArrayList<Node> child) {
+		Child = child;
+	}
+}
+
+class Tree {
+	Node root;
+}
