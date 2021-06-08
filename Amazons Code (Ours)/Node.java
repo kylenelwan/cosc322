a3a5e68db517e6150997e92c9bc2ccad971c87f3
warner1334 <58993802+warner1334@users.noreply.github.com>
2021-06-08 13:06:59 -0700

    Update Node.java

--- Amazons Code (Ours)/Node.java
+++ Amazons Code (Ours)/Node.java
@@ -1,17 +1,37 @@
-/*
- * Sources used:
- * https://www.baeldung.com/java-monte-carlo-tree-search
- * https://www.baeldung.com/java-random-list-element
- */
 package ubc.cosc322;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Random;
 
 public class Node {
 	State state;
 	Node parent;
-	ArrayList<Node> Child;
+	ArrayList<Node> childArray;
+	
+	public Node(Node node) {
+		this.childArray = new ArrayList<>();
+		this.state = node.state;
+		if (node.getParent() != null)
+	            this.parent = node.getParent();
+		ArrayList<Node> childArray = node.getChildArray();
+	    for (Node child : childArray) {
+	          this.childArray.add(new Node(child));
+	    }
+	    
+	}
+	public Node() {
+		this.state = new State();
+		this.childArray = new ArrayList<Node>();
+		
+	}
+	public Node(State state) {
+		this.state = state;
+		this.childArray = new ArrayList<Node>();
+		
+	}
+	
 	public State getState() {
 		return state;
 	}
@@ -24,31 +44,27 @@ public class Node {
 	public void setParent(Node parent) {
 		this.parent = parent;
 	}
-	public ArrayList<Node> getChild() {
-		return Child;
+	public ArrayList<Node> getChildArray() {
+		return childArray;
 	}
-	public void setChild(ArrayList<Node> child) {
-		Child = child;
+	public void setChildArray(ArrayList<Node> childArray) {
+		this.childArray = childArray;
 	}
 	
-	// double check this!!! does it make sense?
-	// should it be a new array list?
 	public Node getRandomChildNode() {
-	    Random rand = new Random();
-	    Node randomNode = Child.get(rand.nextInt(Child.size()));
-		return randomNode;	
-	}
-}
-
-class Tree {
-	Node root;
-
-	public Node getRoot() {
-		return root;
-	}
-	public void setRoot(Node root) {
-		this.root = root;
+		Random rand = new Random();
+		int randomNum = rand.nextInt(childArray.size());
+		Node ranChild = childArray.get(randomNum);
+		return ranChild;
 	}
 	
+	public Node getChildWithMaxScore() {
+		  return Collections.max(this.childArray, Comparator.comparing(c -> {
+	            return c.getState().getVisitCount();
+	        }));
+	}
 	
 }
+
+
+
