38b82e057d242a897b065450fe4325833db73441
warner1334 <58993802+warner1334@users.noreply.github.com>
2021-06-08 13:06:06 -0700

    Update MCTS.java

--- Amazons Code (Ours)/MCTS.java
+++ Amazons Code (Ours)/MCTS.java
@@ -1,64 +1,103 @@
-/*
- * Sources used:
- * https://www.baeldung.com/java-monte-carlo-tree-search
- */
 package ubc.cosc322;
 
 import java.util.ArrayList;
+import java.util.Random;
 
-public class MCTS {
+public class NewMonteCarlo {
+	static final int WIN_SCORE = 10;
+	int level;
+	int opponent;
 	
-	public Board findNextMove(Board board, int playerNo) {
-		int blackPlayer = 1;
-		int whitePlayer = 2;
+	public Move findNextMove(Board board, int playerNo) {
+		opponent = 3 - playerNo;
+		System.out.println("In monte");
+		//board.printState();
 		Tree tree = new Tree();
-		Node root = tree.getRoot();
-		root.getState().setBoard(board);
-		if(playerNo == 1) {
-			root.getState().setPlayerNo(whitePlayer); // note: setPlayerNo is set to "opponent" on the source article
-		} else if (playerNo == 2) {
-			root.getState().setPlayerNo(blackPlayer); 
-		}
-		Node promisingNode = selectPromising(root);
+		Node rootNode = tree.getRoot();
+		rootNode.getState().setBoard(board);
+		rootNode.getState().setPlayerNo(playerNo);
+		//System.out.println(rootNode.childArray.isEmpty());
+		//rootNode.getState().getBoard().printState();
 		long startTime = System.currentTimeMillis();
-		long endTime = startTime + 30*1000; // 30 second time limit
-		while (startTime < endTime) {
+		while((System.currentTimeMillis()-startTime)<1000) {
+			//System.out.println("Phase 1");
+			//Phase 1
+			Node promisingNode = selectPromisingNode(rootNode);
+			//Phase 2
 			if(promisingNode.getState().getBoard().checkGame() == -1) {
-				expand(promisingNode);
+				expandNode(promisingNode);
 			}
-			Node nodeExplore = promisingNode;
-			if (promisingNode.getChild().size() > 0) {
-                nodeExplore = promisingNode.getRandomChildNode();
-            }
-		   // add more
+			//Phase 3
+			Node nodeToExplore = promisingNode;
+			if(promisingNode.getChildArray().size()>0) {
+				nodeToExplore = promisingNode.getRandomChildNode();
+			}
+			
+			int playoutResult = simulateRandomPlayout(nodeToExplore);
+			//System.out.println(playoutResult);
+			backPropogation(nodeToExplore, playoutResult);
 		}
-		return board; // change later
+		//System.out.println(rootNode.getChildArray().size());
+		for(Node testNode: rootNode.getChildArray()) {
+			System.out.println(testNode.state.winScore);
+		}
+		Node winnerNode = rootNode.getChildWithMaxScore();
+		//System.out.println(winnerNode.state.winScore);
+		tree.setRoot(winnerNode);
+		return winnerNode.getState().move;
 	}
 	
-	private Node selectPromising(Node root) {
-		Node node = root;
-		while(node.getChild().size() > 0) {
-			node = UCT.uctFindBestNode(node);
-		}
+	
+	
+	private Node selectPromisingNode(Node rootNode) {
+		Node node = rootNode;
+		
+		 while (node.getChildArray().size() != 0) {
+	            node = UCT.findBestNodeWithUCT(node);
+	     }
 		return node;
 	}
 	
-	private void expand(Node node) {
-		ArrayList<State> allPossibleStates = node.getState().getAllPossibleStates();
-		for (State state : allPossibleStates) {
-			Node newNode = new Node();
+	private void expandNode(Node node) {
+		
+		ArrayList<State> possibleStates = node.getState().getAllPossibleStates();
+		for(State state: possibleStates) {
+			Node newNode = new Node(state);
 			newNode.setParent(node);
 			newNode.getState().setPlayerNo(node.getState().getOpponent());
-	        node.getChild().add(newNode);
+			node.getChildArray().add(newNode);
+		}
+		
+		
+	}
+	private int simulateRandomPlayout(Node node) {
+		Node tempNode = new Node(node);
+		State tempState = tempNode.getState();
+		int boardStatus = tempState.getBoard().checkGame();
+		
+		if(boardStatus == opponent) {
+			tempNode.getParent().getState().setWinScore(Integer.MIN_VALUE);
+			return boardStatus;
+		}
+		
+		while(boardStatus == -1) {
+			tempState.togglePlayer();
+			boardStatus = tempState.randomPlay();
+			//boardStatus = tempState.getBoard().checkGame();
+		}
+	
+		return boardStatus;
+	}
+	
+	private void backPropogation(Node nodeToExplore, int playerNo) {
+		Node tempNode = nodeToExplore;
+		while(tempNode != null) {
+			tempNode.getState().incrementVisit();
+			if(tempNode.getState().getPlayerNo() == playerNo) {
+				tempNode.getState().addScore(WIN_SCORE);
+			}
+			tempNode = tempNode.getParent();
 		}
 	}
-	
-	private int similate(Node node) {
-		return 0;
-	}
-	
-	private void backPropogate(Node nodeExplore, int playerNo) {
-	}
-	
-	
 }
+
