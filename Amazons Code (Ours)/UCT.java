ebdf21eca82bc5126c5976fcb2b27e6950b184b0
warner1334 <58993802+warner1334@users.noreply.github.com>
2021-06-10 12:51:19 -0700

    Update UCT.java

--- Amazons Code (Ours)/UCT.java
+++ Amazons Code (Ours)/UCT.java
@@ -2,13 +2,17 @@ package ubc.cosc322;
 
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Random;
 //heavly copied need to fix and write our own
 public class UCT {
+	static double epsilon = 1e-6;
+	static Random r = new Random();
 	public static double uctValue(int totalVisit, double nodeWinScore, int nodeVisit) {
 		if(nodeVisit  == 0) {
 			return Integer.MAX_VALUE;
 		}
-		return ((double)nodeWinScore/(double)nodeVisit)+1.41*Math.sqrt(Math.log(totalVisit)/(double)nodeVisit);
+		return ((double)nodeWinScore/(double)nodeVisit)+ 2*Math.sqrt(Math.log(totalVisit)/(double)nodeVisit);
+		//return   nodeWinScore / (nodeVisit + epsilon) + Math.sqrt(Math.log(totalVisit+1) / (totalVisit + epsilon)) + r.nextDouble() * epsilon;
 	}
 	public static Node findBestNodeWithUCT(Node node) {
 		int parentVisit = node.getState().getVisitCount();
