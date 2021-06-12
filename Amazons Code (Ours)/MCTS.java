5ba5d494c0bb839f6097237941e4e7bdf72b24eb
warner1334 <58993802+warner1334@users.noreply.github.com>
2021-06-12 16:34:24 -0700

    Update MCTS.java

--- Amazons Code (Ours)/MCTS.java
+++ Amazons Code (Ours)/MCTS.java
@@ -3,62 +3,58 @@ package ubc.cosc322;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class NewMonteCarlo {
-	public int whiteWins = 0;
-	public int blackWins = 0;
+public class NewMonteCarlo2 {
+
 	static final int WIN_SCORE = 10;
 	int level;
 	int opponent;
 	
 	public Move findNextMove(Board board, int playerNo) {
 		opponent = 3 - playerNo;
-		System.out.println("In monte");
 		Tree tree = new Tree();
+		Node maxNode = null;
 		Node rootNode = tree.getRoot();
 		rootNode.getState().setBoard(board);
 		rootNode.getState().setPlayerNo(opponent);
 		long startTime = System.currentTimeMillis();
-		int count = 0;
-		while((System.currentTimeMillis()-startTime) < 28500) {
-		
-			//System.out.println("Phase 1");
-			//Phase 1
-			Node promisingNode = selectPromisingNode(rootNode);
-			//Phase 2
-			if(promisingNode.getState().getBoard().checkGame() == -1) {
-				expandNode(promisingNode);
-			}
+			while((System.currentTimeMillis()-startTime) < 27000) {
 			
-			//Phase 3
-			//System.out.println("HERE");
-			Node nodeToExplore = new Node(promisingNode);
-			//nodeToExplore.state.board.printState();
-			if(promisingNode.getChildArray().size()>0) {
-				nodeToExplore = promisingNode.getRandomChildNode();
+				//System.out.println("Phase 1");
+				//Phase 1
+				Node promisingNode = selectPromisingNode(rootNode);
+				//Phase 2
+				if(promisingNode.getState().getBoard().checkGame() == -1) {
+					expandNode(promisingNode);
+				}
+				
+				//Phase 3
+				
+				Node nodeToExplore = new Node(promisingNode);
+				
+				if(promisingNode.getChildArray().size()>0) {
+					nodeToExplore = promisingNode.getRandomChildNode();
+				}
+				int playoutResult = simulateRandomPlayout(nodeToExplore);
+				//Phase 4
+				backPropogation(nodeToExplore, playoutResult);
 			}
-			//nodeToExplore.state.board.printState();
-			int playoutResult = simulateRandomPlayout(nodeToExplore);
-			
-			backPropogation(nodeToExplore, playoutResult);
-			count++;
-	}
 		
 		System.out.println("Root node visits");
 		System.out.println(rootNode.state.visitCount);
 		System.out.println("Possible moves");
-		ArrayList<State> possibleStates = rootNode.getState().getAllPossibleStates();
-		System.out.println(possibleStates.size());
-		System.out.println("Child ArraySize");
 		System.out.println(rootNode.childArray.size());
 		Node winnerNode = rootNode.getChildWithMaxScore();
 		System.out.println("Winner Node Visits");
 		System.out.println(winnerNode.state.visitCount);
+		System.out.println("PlayNo");
+		System.out.println(winnerNode.state.playerNo);;
 		System.out.println("Final score");
 		System.out.println(winnerNode.state.winScore);
 		System.out.println("Black Wins");
 		System.out.println(winnerNode.state.winScoreBlack);
 		System.out.println("White Wins");
 		System.out.println(winnerNode.state.winScoreWhite);
+		winnerNode.state.board.printState();
 		tree.setRoot(winnerNode);
 		return winnerNode.getState().move;
 	}
@@ -89,20 +85,13 @@ public class NewMonteCarlo {
 		State tempState = tempNode.getState();
 		int boardStatus = tempState.getBoard().checkGame();
 
-		
 		if(boardStatus == opponent) {
 			tempNode.getParent().getState().setWinScore(Integer.MIN_VALUE);
 			return boardStatus;
 		}
-		
 		while(boardStatus == -1) {
 			boardStatus = tempState.randomPlay();
 			tempState.togglePlayer();
-		}
-		if(boardStatus == 1) {
-			blackWins++;
-		}else if(boardStatus == 2) {
-			whiteWins++;
 		}
 		return boardStatus;
 	}
@@ -118,5 +107,8 @@ public class NewMonteCarlo {
 			tempNode = tempNode.getParent();
 		}
 	}
+	
+	
+
 }
 
